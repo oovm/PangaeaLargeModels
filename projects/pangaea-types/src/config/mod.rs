3da58f5710a9e0f57e08522713e42cbbe46f061c
Aster <aster@vers.site>
2024-06-18 15:33:21 +0800

    Rename the project to pangaea

--- projects/pangaea-types/src/config/mod.rs
+++ projects/pangaea-types/src/config/mod.rs
@@ -1,8 +1,8 @@
 use schemars::{JsonSchema, schema_for};
 
 #[derive(JsonSchema)]
-pub struct DiffuserMatrixConfig {
-    pub my_int: i32,
+pub struct DeusMatrixConfig {
+    pub civit_key: i32,
     pub my_bool: bool,
     pub my_nullable_enum: Option<DatabaseConfig>,
 }
@@ -13,8 +13,11 @@ pub enum DatabaseConfig {
     StructVariant { floats: Vec<f32> },
 }
 
+
+
+
 #[test]
 fn test() {
-    let schema = schema_for!(DiffuserMatrixConfig);
+    let schema = schema_for!(DeusMatrixConfig);
     println!("{}", serde_json::to_string_pretty(&schema).unwrap());
 }
